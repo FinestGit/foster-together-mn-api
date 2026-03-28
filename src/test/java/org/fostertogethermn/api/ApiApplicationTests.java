15c64b96a0f39cb389ddda2284773e51fa2765d3
Chase Rogness <chaserogness@gmail.com>
2026-03-28 13:45:16 -0500

    Got basic Spring Boot application working

---
+++ src/test/java/org/fostertogethermn/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fostertogethermn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
